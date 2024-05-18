6f981aebb5995bb00f7bf6087515ae594dee2f85
C3RJp <juancrrr4@gmail.com>
2024-05-17 19:59:07 -0500

    Cambiar nombre de archivo (punto)env --> .env

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -9,50 +9,84 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    // public function test_first(): void
+    // public function test_validar_status_peticion_get_index(): void
     // {
-    //     $resp = $this->get('/');
-    //     $resp-> assertStatus(200);
-
-    //     $resp1 = $this->get('administrador');
-    //     $resp1-> assertStatus(200);
-
-    //     $resp2 = $this->get('empleado');
-    //     $resp2-> assertStatus(200);
+    //     $response = $this->get('/');
+    //     $response -> assertOk();
+    //     $response->assertViewIs('index');
     // }
-    
-    // Public function test_client_managment(): void
+    // public function test_validar_status_peticion_get_administrador(): void
+    // {
+    //     $response = $this->get('administrador');
+    //     $response -> assertOk();
+    //     $response->assertViewIs('administrador');
+    // }
+    // public function test_validar_status_peticion_get_empleado(): void
+    // {
+    //     $response = $this->get('empleado');
+    //     $response -> assertOk();
+    //     $response->assertViewIs('empleado');
+    // }
+
+    // Public function test_validar_status_peticiones_get_gestionEmpleado(): void
     // {
     //     $response = $this->get('registroe');
-    //     $response-> assertStatus(200);
+    //     $response-> assertOk();
+    //     $response->assertViewIs('registroe');
 
     //     $response1 = $this->get('consultae');
-    //     $response1-> assertStatus(200);
+    //     $response1-> assertOk();
+    //     $response1->assertViewIs('consultae');
 
     //     $response2 = $this->get('actualizacione');
-    //     $response2-> assertStatus(200);
+    //     $response2-> assertOk();
+    //     $response2->assertViewIs('actualizacione');
 
     //     $response3 = $this->get('borrare');
-    //     $response3-> assertStatus(200);
+    //     $response3-> assertOk();
+    //     $response3->assertViewIs('borrare');
     // }
 
-    Public function test_post_empleados()
-    {
-        $datos=
-        [
-            'cargo'=>"empleado",
-            'cedula'=>"126",
-            'nombres'=>"emp2",
-            'apellidos'=>"emp2",
-            'direccion'=>"Cra126",
-            'celular'=>"312203",
-            'correo'=>"emp2@mail.com",
-            'clave'=>"126"
-        ];
+    // Public function test_post_registro_empleados()
+    // {
+    //     $datos=
+    //     [
+    //         'cargo'=>"empleado",
+    //         'cedula'=>"128",
+    //         'nombres'=>"emp128",
+    //         'apellidos'=>"emp128",
+    //         'direccion'=>"Cra128",
+    //         'celular'=>"320128",
+    //         'correo'=>"emp128@mail.com",
+    //         'clave'=>"128"
+    //     ];
 
-        $response = $this->post('registroe',$datos);
-        $response = $this->withHeaders(['X-CSRF-TOKEN' => csrf_token(),])->post('registroe', $datos);
-        $response->assertFound();
+    //     $response = $this->post('registroe',$datos);
+    //     $response = $this->withHeaders(['X-CSRF-TOKEN' => csrf_token(),])->post('registroe', $datos);
+    //     $response->assertFound();
+    // }
+
+    // public function test_post_consulta_empleado():void
+    // {    
+    //     $datosB=
+    //     [            
+    //         'cedula'=>"128",
+    //         'nombres'=>"emp128",
+    //         'apellidos'=>"emp128",
+    //         'direccion'=>"Cra128",
+    //         'celular'=>"320128",
+    //         'correo'=>"emp128@mail.com"               
+    //     ];
+    //         $response = $this->withHeaders(['X-CSRF-TOKEN' => csrf_token(),])->post('consultae', $datosB);
+    //         $this->assertDatabaseHas('empleados', ['cedula' => 128]);        
+    //     }
+
+        
+
+        // public function test_post_borrare():void
+        // {
+        //     $this->withHeaders(['X-CSRF-TOKEN' => csrf_token(),])->post('borrare', ['cedula'=>126]);
+        //     $this->assertDatabaseCount('empleados', 5);
+        // }
         
     }
-}
